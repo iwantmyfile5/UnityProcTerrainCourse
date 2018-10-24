e006b8581e1820af8a7e925d365e61aaf04bc22e
iwantmyfile5 <iwantmyfile5@users.noreply.github.com>
2018-10-23 20:32:56 -0400

    Setting up editor skeleton S2 L10
    Editor skeleton was created, but no functionality added. New scene with empty terrain created so we have something to modify later on. Imported EditorGUITable package for creating GUI in Editor. From S:2 L:10

---
+++ Procedural Terrain Course/Assets/Scripts/CustomTerrain.cs
@@ -0,0 +1,64 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+[ExecuteInEditMode]
+
+public class CustomTerrain : MonoBehaviour {
+
+    void Awake()
+    {
+        //Get tag database
+        SerializedObject tagManager = new SerializedObject(
+                                AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+        SerializedProperty tagsProp = tagManager.FindProperty("tags");
+
+        //Add new tags
+        AddTag(tagsProp, "Terrain");
+        AddTag(tagsProp, "Cloud");
+        AddTag(tagsProp, "Shore");
+
+        //Apply tag changes to tag database
+        tagManager.ApplyModifiedProperties();
+
+        //Apply tag to this game object
+        this.gameObject.tag = "Terrain";
+    }
+
+    void AddTag(SerializedProperty tagsProp, string newTag)
+    {
+        bool found = false;
+
+        //ensure the tag doesn't already exist
+        for (int i = 0; i < tagsProp.arraySize; i++)
+        {
+            SerializedProperty t = tagsProp.GetArrayElementAtIndex(i);
+            if(t.stringValue.Equals(newTag))
+            {
+                found = true;
+                break;
+            }
+        }
+
+        //add the new tag
+        if(!found)
+        {
+            tagsProp.InsertArrayElementAtIndex(0);
+            SerializedProperty newTagProp = tagsProp.GetArrayElementAtIndex(0);
+            newTagProp.stringValue = newTag;
+        }
+
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
